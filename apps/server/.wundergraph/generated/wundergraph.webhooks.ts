eaa9f8d073f963e84c4fa912117ffacd0acb8f0a
Jem <0x0xjem@gmail.com>
2023-05-03 12:11:47 +0400

    Remove generated files

--- apps/server/.wundergraph/generated/wundergraph.webhooks.ts
+++
@@ -1,8 +0,0 @@
-import type { WebhookConfiguration } from "@wundergraph/sdk/server";
-import type { InternalClient } from "./wundergraph.internal.client";
-import type { InternalOperationsClient } from "./wundergraph.internal.operations.client";
-import { createWebhookFactory } from "@wundergraph/sdk/server";
-
-export type WebhooksConfig = {};
-
-export const createWebhook = createWebhookFactory<InternalOperationsClient, InternalClient>();
