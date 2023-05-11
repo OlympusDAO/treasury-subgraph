3e6d3732cb7b76efda94cd4ad5067fe314878364
Jem <0x0xjem@gmail.com>
2023-05-11 13:24:23 +0400

    Tests for earliest/latest/atBlock

--- apps/server/tests/tokenRecords.test.ts
+++ apps/server/tests/tokenRecords.test.ts
@@ -2,6 +2,7 @@ import { addDays } from "date-fns";
 import { createTestServer } from "../.wundergraph/generated/testing";
 import { getISO8601DateString } from "./dateHelper";
 import { CHAIN_ARBITRUM, CHAIN_ETHEREUM, CHAIN_FANTOM, CHAIN_POLYGON } from "../.wundergraph/constants";
+import { TokenRecordsResponseData } from "../.wundergraph/generated/models";
 
 const wg = createTestServer();
 
@@ -15,6 +16,26 @@ afterAll(async () => {
 
 const getStartDate = (days: number = -5): string => {
   return getISO8601DateString(addDays(new Date(), days));
+}
+
+type TokenRecord = TokenRecordsResponseData["treasuryEthereum_tokenRecords"][0];
+
+const getFirstRecordByChain = (records: TokenRecord[] | undefined, chain: string): TokenRecord | null => {
+  if (!records) {
+    return null;
+  }
+
+  const filteredRecords = records.filter((record) => record.blockchain === chain);
+  return filteredRecords.length > 0 ? filteredRecords[0] : null;
+}
+
+const getFirstRecordByChainByDate = (records: TokenRecord[] | undefined, chain: string, date: string): TokenRecord | null => {
+  if (!records) {
+    return null;
+  }
+
+  const filteredRecords = records.filter((record) => record.blockchain === chain && record.date == date);
+  return filteredRecords.length > 0 ? filteredRecords[0] : null;
 }
 
 describe("paginated", () => {
@@ -43,6 +64,180 @@ describe("paginated", () => {
     const recordLength = records ? records.length : 0;
     expect(recordLength).toBeGreaterThan(0);
   });
+
+  test("returns blockchain property for Arbitrum", async () => {
+    const result = await wg.client().query({
+      operationName: "paginated/tokenRecords",
+      input: {
+        startDate: getStartDate(),
+      }
+    });
+
+    const records = result.data;
+    const filteredRecords = records ? records.filter((record) => record.blockchain === CHAIN_ARBITRUM) : [];
+    expect(filteredRecords.length).toBeGreaterThan(0);
+  });
+
+  test("returns blockchain property for Ethereum", async () => {
+    const result = await wg.client().query({
+      operationName: "paginated/tokenRecords",
+      input: {
+        startDate: getStartDate(),
+      }
+    });
+
+    const records = result.data;
+    const filteredRecords = records ? records.filter((record) => record.blockchain === CHAIN_ETHEREUM) : [];
+    expect(filteredRecords.length).toBeGreaterThan(0);
+  });
+
+  test("returns blockchain property for Fantom", async () => {
+    const result = await wg.client().query({
+      operationName: "paginated/tokenRecords",
+      input: {
+        startDate: getStartDate(),
+      }
+    });
+
+    const records = result.data;
+    const filteredRecords = records ? records.filter((record) => record.blockchain === CHAIN_FANTOM) : [];
+    expect(filteredRecords.length).toBeGreaterThan(0);
+  });
+
+  test("returns blockchain property for Polygon", async () => {
+    const result = await wg.client().query({
+      operationName: "paginated/tokenRecords",
+      input: {
+        startDate: getStartDate(),
+      }
+    });
+
+    const records = result.data;
+    const filteredRecords = records ? records.filter((record) => record.blockchain === CHAIN_POLYGON) : [];
+    expect(filteredRecords.length).toBeGreaterThan(0);
+  });
+});
+
+describe("latest", () => {
+  test("returns the latest results for each chain", async () => {
+    // Grab the results from the raw operation
+    const rawResult = await wg.client().query({
+      operationName: "tokenRecordsLatest",
+    });
+
+    // Raw data has an array property for each chain
+    const arbitrumRawResult = rawResult.data?.treasuryArbitrum_tokenRecords[0];
+    const ethereumRawResult = rawResult.data?.treasuryEthereum_tokenRecords[0];
+    const fantomRawResult = rawResult.data?.treasuryFantom_tokenRecords[0];
+    const polygonRawResult = rawResult.data?.treasuryPolygon_tokenRecords[0];
+
+    // Grab the results from the latest operation
+    const result = await wg.client().query({
+      operationName: "latest/tokenRecords",
+    });
+
+    // Latest records is collapsed into a flat array
+    const records = result.data;
+    const arbitrumResult = getFirstRecordByChain(records, CHAIN_ARBITRUM);
+    const ethereumResult = getFirstRecordByChain(records, CHAIN_ETHEREUM);
+    const fantomResult = getFirstRecordByChain(records, CHAIN_FANTOM);
+    const polygonResult = getFirstRecordByChain(records, CHAIN_POLYGON);
+
+    // Check that the block is the same
+    expect(arbitrumResult?.block).toEqual(arbitrumRawResult?.block);
+    expect(ethereumResult?.block).toEqual(ethereumRawResult?.block);
+    expect(fantomResult?.block).toEqual(fantomRawResult?.block);
+    expect(polygonResult?.block).toEqual(polygonRawResult?.block);
+
+    // Check that the array length is the same
+    const recordLength = records ? records.length : 0;
+    expect(recordLength).toEqual(4);
+  });
+});
+
+describe("earliest", () => {
+  test("returns the earliest results for each chain", async () => {
+    // Grab the results from the raw operation
+    const rawResult = await wg.client().query({
+      operationName: "tokenRecordsEarliest",
+    });
+
+    // Raw data has an array property for each chain
+    const arbitrumRawResult = rawResult.data?.treasuryArbitrum_tokenRecords[0];
+    const ethereumRawResult = rawResult.data?.treasuryEthereum_tokenRecords[0];
+    const fantomRawResult = rawResult.data?.treasuryFantom_tokenRecords[0];
+    const polygonRawResult = rawResult.data?.treasuryPolygon_tokenRecords[0];
+
+    // Grab the results from the earliest operation
+    const result = await wg.client().query({
+      operationName: "earliest/tokenRecords",
+    });
+
+    // Latest records is collapsed into a flat array
+    const records = result.data;
+    const arbitrumResult = getFirstRecordByChain(records, CHAIN_ARBITRUM);
+    const ethereumResult = getFirstRecordByChain(records, CHAIN_ETHEREUM);
+    const fantomResult = getFirstRecordByChain(records, CHAIN_FANTOM);
+    const polygonResult = getFirstRecordByChain(records, CHAIN_POLYGON);
+
+    // Check that the block is the same
+    expect(arbitrumResult?.block).toEqual(arbitrumRawResult?.block);
+    expect(ethereumResult?.block).toEqual(ethereumRawResult?.block);
+    expect(fantomResult?.block).toEqual(fantomRawResult?.block);
+    expect(polygonResult?.block).toEqual(polygonRawResult?.block);
+
+    // Check that the array length is the same
+    const recordLength = records ? records.length : 0;
+    expect(recordLength).toEqual(4);
+  });
+});
+
+describe("atBlock", () => {
+  test("returns the results for each chain at the specified block", async () => {
+    const startDate = getISO8601DateString(addDays(new Date(), -1));
+
+    // Grab the results for the previous day (hence not the result of the latest query)
+    const rawResult = await wg.client().query({
+      operationName: "paginated/tokenRecords",
+      input: {
+        startDate: startDate,
+      },
+    });
+
+    // Raw data has an array property for each chain
+    const arbitrumRawResult = getFirstRecordByChainByDate(rawResult.data, CHAIN_ARBITRUM, startDate);
+    const arbitrumRawBlock = arbitrumRawResult?.block || "0";
+    const ethereumRawResult = getFirstRecordByChainByDate(rawResult.data, CHAIN_ETHEREUM, startDate);
+    const ethereumRawBlock = ethereumRawResult?.block || "0";
+    const fantomRawResult = getFirstRecordByChainByDate(rawResult.data, CHAIN_FANTOM, startDate);
+    const fantomRawBlock = fantomRawResult?.block || "0";
+    const polygonRawResult = getFirstRecordByChainByDate(rawResult.data, CHAIN_POLYGON, startDate);
+    const polygonRawBlock = polygonRawResult?.block || "0";
+
+    // Grab the results from the earliest operation
+    const result = await wg.client().query({
+      operationName: "atBlock/tokenRecords",
+      input: {
+        arbitrumBlock: parseInt(arbitrumRawBlock),
+        ethereumBlock: parseInt(ethereumRawBlock),
+        fantomBlock: parseInt(fantomRawBlock),
+        polygonBlock: parseInt(polygonRawBlock),
+      }
+    });
+
+    // Latest records is collapsed into a flat array
+    const records = result.data;
+    const arbitrumResult = getFirstRecordByChain(records, CHAIN_ARBITRUM);
+    const ethereumResult = getFirstRecordByChain(records, CHAIN_ETHEREUM);
+    const fantomResult = getFirstRecordByChain(records, CHAIN_FANTOM);
+    const polygonResult = getFirstRecordByChain(records, CHAIN_POLYGON);
+
+    // Check that the block is the same
+    expect(arbitrumResult?.block).toEqual(arbitrumRawBlock);
+    expect(ethereumResult?.block).toEqual(ethereumRawBlock);
+    expect(fantomResult?.block).toEqual(fantomRawBlock);
+    expect(polygonResult?.block).toEqual(polygonRawBlock);
+  });
 });
 
 // describe("converts to numbers", () => {
@@ -59,57 +254,3 @@ describe("paginated", () => {
 //     expect(typeof record?.balance).toBe("number");
 //   })
 // });
-
-describe("blockchain property", () => {
-  test(CHAIN_ARBITRUM, async () => {
-    const result = await wg.client().query({
-      operationName: "paginated/tokenRecords",
-      input: {
-        startDate: getStartDate(),
-      }
-    });
-
-    const records = result.data;
-    const filteredRecords = records ? records.filter((record) => record.blockchain === CHAIN_ARBITRUM) : [];
-    expect(filteredRecords.length).toBeGreaterThan(0);
-  });
-
-  test(CHAIN_ETHEREUM, async () => {
-    const result = await wg.client().query({
-      operationName: "paginated/tokenRecords",
-      input: {
-        startDate: getStartDate(),
-      }
-    });
-
-    const records = result.data;
-    const filteredRecords = records ? records.filter((record) => record.blockchain === CHAIN_ETHEREUM) : [];
-    expect(filteredRecords.length).toBeGreaterThan(0);
-  });
-
-  test(CHAIN_FANTOM, async () => {
-    const result = await wg.client().query({
-      operationName: "paginated/tokenRecords",
-      input: {
-        startDate: getStartDate(),
-      }
-    });
-
-    const records = result.data;
-    const filteredRecords = records ? records.filter((record) => record.blockchain === CHAIN_FANTOM) : [];
-    expect(filteredRecords.length).toBeGreaterThan(0);
-  });
-
-  test(CHAIN_POLYGON, async () => {
-    const result = await wg.client().query({
-      operationName: "paginated/tokenRecords",
-      input: {
-        startDate: getStartDate(),
-      }
-    });
-
-    const records = result.data;
-    const filteredRecords = records ? records.filter((record) => record.blockchain === CHAIN_POLYGON) : [];
-    expect(filteredRecords.length).toBeGreaterThan(0);
-  });
-});
--- apps/server/tests/tokenSupplies.test.ts
+++ apps/server/tests/tokenSupplies.test.ts
@@ -2,6 +2,7 @@ import { addDays } from "date-fns";
 import { createTestServer } from "../.wundergraph/generated/testing";
 import { getISO8601DateString } from "./dateHelper";
 import { CHAIN_ARBITRUM, CHAIN_ETHEREUM, CHAIN_FANTOM, CHAIN_POLYGON } from "../.wundergraph/constants";
+import { TokenRecordsResponseData, TokenSuppliesResponseData } from "../.wundergraph/generated/models";
 
 const wg = createTestServer();
 
@@ -15,6 +16,26 @@ afterAll(async () => {
 
 const getStartDate = (days: number = -5): string => {
   return getISO8601DateString(addDays(new Date(), days));
+}
+
+type TokenSupply = TokenSuppliesResponseData["treasuryEthereum_tokenSupplies"][0];
+
+const getFirstRecordByChain = (records: TokenSupply[] | undefined, chain: string): TokenSupply | null => {
+  if (!records) {
+    return null;
+  }
+
+  const filteredRecords = records.filter((record) => record.blockchain === chain);
+  return filteredRecords.length > 0 ? filteredRecords[0] : null;
+}
+
+const getFirstBlockByChainByDate = (records: TokenSupply[] | undefined, chain: string, date: string): string | undefined => {
+  if (!records) {
+    return undefined;
+  }
+
+  const filteredRecords = records.filter((record) => record.blockchain === chain && record.date == date);
+  return filteredRecords.length > 0 ? filteredRecords[0].block : undefined;
 }
 
 describe("paginated", () => {
@@ -43,6 +64,182 @@ describe("paginated", () => {
     const recordLength = records ? records.length : 0;
     expect(recordLength).toBeGreaterThan(0);
   });
+
+  test("returns blockchain property for Arbitrum", async () => {
+    const result = await wg.client().query({
+      operationName: "paginated/tokenSupplies",
+      input: {
+        startDate: getStartDate(),
+      }
+    });
+
+    const records = result.data;
+    const filteredRecords = records ? records.filter((record) => record.blockchain === CHAIN_ARBITRUM) : [];
+    expect(filteredRecords.length).toBeGreaterThan(0);
+  });
+
+  test("returns blockchain property for Ethereum", async () => {
+    const result = await wg.client().query({
+      operationName: "paginated/tokenSupplies",
+      input: {
+        startDate: getStartDate(),
+      }
+    });
+
+    const records = result.data;
+    const filteredRecords = records ? records.filter((record) => record.blockchain === CHAIN_ETHEREUM) : [];
+    expect(filteredRecords.length).toBeGreaterThan(0);
+  });
+
+  test("returns blockchain property for Fantom", async () => {
+    const result = await wg.client().query({
+      operationName: "paginated/tokenSupplies",
+      input: {
+        startDate: getStartDate(),
+      }
+    });
+
+    const records = result.data;
+    const filteredRecords = records ? records.filter((record) => record.blockchain === CHAIN_FANTOM) : [];
+    expect(filteredRecords.length).toBe(0); // 0 TokenSupply on this blockchain
+  });
+
+  test("returns blockchain property for Polygon", async () => {
+    const result = await wg.client().query({
+      operationName: "paginated/tokenSupplies",
+      input: {
+        startDate: getStartDate(),
+      }
+    });
+
+    const records = result.data;
+    const filteredRecords = records ? records.filter((record) => record.blockchain === CHAIN_POLYGON) : [];
+    expect(filteredRecords.length).toBe(0); // 0 TokenSupply on this blockchain
+  });
+});
+
+describe("latest", () => {
+  test("returns the latest results for each chain", async () => {
+    // Grab the results from the raw operation
+    const rawResult = await wg.client().query({
+      operationName: "tokenSuppliesLatest",
+    });
+
+    // Raw data has an array property for each chain
+    const arbitrumRawResult = rawResult.data?.treasuryArbitrum_tokenSupplies[0];
+    const ethereumRawResult = rawResult.data?.treasuryEthereum_tokenSupplies[0];
+    const fantomRawResult = rawResult.data?.treasuryFantom_tokenSupplies[0];
+    const polygonRawResult = rawResult.data?.treasuryPolygon_tokenSupplies[0];
+
+    // Calculate the expected count based on how many of the raw results were defined. This is because there may not be TokenSupply records on every chain.
+    const expectedCount = [arbitrumRawResult, ethereumRawResult, fantomRawResult, polygonRawResult].filter((result) => result !== undefined).length;
+
+    // Grab the results from the latest operation
+    const result = await wg.client().query({
+      operationName: "latest/tokenSupplies",
+    });
+
+    // Latest records is collapsed into a flat array
+    const records = result.data;
+    const arbitrumResult = getFirstRecordByChain(records, CHAIN_ARBITRUM);
+    const ethereumResult = getFirstRecordByChain(records, CHAIN_ETHEREUM);
+    const fantomResult = getFirstRecordByChain(records, CHAIN_FANTOM);
+    const polygonResult = getFirstRecordByChain(records, CHAIN_POLYGON);
+
+    // Check that the block is the same
+    expect(arbitrumResult?.block).toEqual(arbitrumRawResult?.block);
+    expect(ethereumResult?.block).toEqual(ethereumRawResult?.block);
+    expect(fantomResult?.block).toEqual(fantomRawResult?.block);
+    expect(polygonResult?.block).toEqual(polygonRawResult?.block);
+
+    // Check that the array length is the same
+    const recordLength = records ? records.length : 0;
+    expect(recordLength).toEqual(expectedCount);
+  });
+});
+
+describe("earliest", () => {
+  test("returns the earliest results for each chain", async () => {
+    // Grab the results from the raw operation
+    const rawResult = await wg.client().query({
+      operationName: "tokenSuppliesEarliest",
+    });
+
+    // Raw data has an array property for each chain
+    const arbitrumRawResult = rawResult.data?.treasuryArbitrum_tokenSupplies[0];
+    const ethereumRawResult = rawResult.data?.treasuryEthereum_tokenSupplies[0];
+    const fantomRawResult = rawResult.data?.treasuryFantom_tokenSupplies[0];
+    const polygonRawResult = rawResult.data?.treasuryPolygon_tokenSupplies[0];
+
+    // Calculate the expected count based on how many of the raw results were defined. This is because there may not be TokenSupply records on every chain.
+    const expectedCount = [arbitrumRawResult, ethereumRawResult, fantomRawResult, polygonRawResult].filter((result) => result !== undefined).length;
+
+    // Grab the results from the earliest operation
+    const result = await wg.client().query({
+      operationName: "earliest/tokenSupplies",
+    });
+
+    // Latest records is collapsed into a flat array
+    const records = result.data;
+    const arbitrumResult = getFirstRecordByChain(records, CHAIN_ARBITRUM);
+    const ethereumResult = getFirstRecordByChain(records, CHAIN_ETHEREUM);
+    const fantomResult = getFirstRecordByChain(records, CHAIN_FANTOM);
+    const polygonResult = getFirstRecordByChain(records, CHAIN_POLYGON);
+
+    // Check that the block is the same
+    expect(arbitrumResult?.block).toEqual(arbitrumRawResult?.block);
+    expect(ethereumResult?.block).toEqual(ethereumRawResult?.block);
+    expect(fantomResult?.block).toEqual(fantomRawResult?.block);
+    expect(polygonResult?.block).toEqual(polygonRawResult?.block);
+
+    // Check that the array length is the same
+    const recordLength = records ? records.length : 0;
+    expect(recordLength).toEqual(expectedCount);
+  });
+});
+
+describe("atBlock", () => {
+  test("returns the results for each chain at the specified block", async () => {
+    const startDate = getISO8601DateString(addDays(new Date(), -1));
+
+    // Grab the results for the previous day (hence not the result of the latest query)
+    const rawResult = await wg.client().query({
+      operationName: "paginated/tokenSupplies",
+      input: {
+        startDate: startDate,
+      },
+    });
+
+    // Raw data has an array property for each chain
+    const arbitrumRawBlock = getFirstBlockByChainByDate(rawResult.data, CHAIN_ARBITRUM, startDate);
+    const ethereumRawBlock = getFirstBlockByChainByDate(rawResult.data, CHAIN_ETHEREUM, startDate);
+    const fantomRawBlock = getFirstBlockByChainByDate(rawResult.data, CHAIN_FANTOM, startDate);
+    const polygonRawBlock = getFirstBlockByChainByDate(rawResult.data, CHAIN_POLYGON, startDate);
+
+    // Grab the results from the earliest operation
+    const result = await wg.client().query({
+      operationName: "atBlock/tokenSupplies",
+      input: {
+        arbitrumBlock: parseInt(arbitrumRawBlock || "0"),
+        ethereumBlock: parseInt(ethereumRawBlock || "0"),
+        fantomBlock: parseInt(fantomRawBlock || "0"),
+        polygonBlock: parseInt(polygonRawBlock || "0"),
+      }
+    });
+
+    // Latest records is collapsed into a flat array
+    const records = result.data;
+    const arbitrumResult = getFirstRecordByChain(records, CHAIN_ARBITRUM);
+    const ethereumResult = getFirstRecordByChain(records, CHAIN_ETHEREUM);
+    const fantomResult = getFirstRecordByChain(records, CHAIN_FANTOM);
+    const polygonResult = getFirstRecordByChain(records, CHAIN_POLYGON);
+
+    // Check that the block is the same
+    expect(arbitrumResult?.block).toEqual(arbitrumRawBlock);
+    expect(ethereumResult?.block).toEqual(ethereumRawBlock);
+    expect(fantomResult?.block).toEqual(fantomRawBlock);
+    expect(polygonResult?.block).toEqual(polygonRawBlock);
+  });
 });
 
 // describe("converts to numbers", () => {
@@ -59,57 +256,3 @@ describe("paginated", () => {
 //     expect(typeof record?.balance).toBe("number");
 //   })
 // });
-
-describe("blockchain property", () => {
-  test(CHAIN_ARBITRUM, async () => {
-    const result = await wg.client().query({
-      operationName: "paginated/tokenSupplies",
-      input: {
-        startDate: getStartDate(),
-      }
-    });
-
-    const records = result.data;
-    const filteredRecords = records ? records.filter((record) => record.blockchain === CHAIN_ARBITRUM) : [];
-    expect(filteredRecords.length).toBeGreaterThan(0);
-  });
-
-  test(CHAIN_ETHEREUM, async () => {
-    const result = await wg.client().query({
-      operationName: "paginated/tokenSupplies",
-      input: {
-        startDate: getStartDate(),
-      }
-    });
-
-    const records = result.data;
-    const filteredRecords = records ? records.filter((record) => record.blockchain === CHAIN_ETHEREUM) : [];
-    expect(filteredRecords.length).toBeGreaterThan(0);
-  });
-
-  test(CHAIN_FANTOM, async () => {
-    const result = await wg.client().query({
-      operationName: "paginated/tokenSupplies",
-      input: {
-        startDate: getStartDate(),
-      }
-    });
-
-    const records = result.data;
-    const filteredRecords = records ? records.filter((record) => record.blockchain === CHAIN_FANTOM) : [];
-    expect(filteredRecords.length).toBe(0); // 0 TokenSupply on this blockchain
-  });
-
-  test(CHAIN_POLYGON, async () => {
-    const result = await wg.client().query({
-      operationName: "paginated/tokenSupplies",
-      input: {
-        startDate: getStartDate(),
-      }
-    });
-
-    const records = result.data;
-    const filteredRecords = records ? records.filter((record) => record.blockchain === CHAIN_POLYGON) : [];
-    expect(filteredRecords.length).toBe(0); // 0 TokenSupply on this blockchain
-  });
-});
