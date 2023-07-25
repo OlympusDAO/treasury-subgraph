1f2789bfbcd90316d904d4dbe967eec60415a0ef
Jem <0x0xjem@gmail.com>
2023-07-25 12:29:09 +0400

    Better enum values for supply categories

--- apps/server/.wundergraph/constants.ts
+++ apps/server/.wundergraph/constants.ts
@@ -25,16 +25,16 @@ export const TOKEN_SUPPLY_TYPE_LENDING = "Lending";
  * If adding any new categories, be sure to update the getSupplyCategories function.
  */
 export enum TokenSupplyCategories {
-  BONDS_DEPOSITS = "OHM Bonds (Burnable Deposits)",
-  BONDS_PREMINTED = "OHM Bonds (Pre-minted)",
-  BONDS_VESTING_DEPOSITS = "OHM Bonds (Vesting Deposits)",
-  BONDS_VESTING_TOKENS = "OHM Bonds (Vesting Tokens)",
-  LIQUIDITY = "Liquidity",
-  BOOSTED_LIQUIDITY_VAULT = "Boosted Liquidity Vault",
-  OFFSET = "Manual Offset",
-  TOTAL_SUPPLY = "Total Supply",
+  BONDS_DEPOSITS = "BondsDeposits",
+  BONDS_PREMINTED = "BondsPreminted",
+  BONDS_VESTING_DEPOSITS = "BondsDepositsVesting",
+  BONDS_VESTING_TOKENS = "BondsTokensVesting",
+  BOOSTED_LIQUIDITY_VAULT = "BoostedLiquidityVault",
+  LENDING = "LendingMarkets",
+  LIQUIDITY = "ProtocolOwnedLiquidity",
+  MIGRATION_OFFSET = "MigrationOffset",
+  TOTAL_SUPPLY = "TotalSupply",
   TREASURY = "Treasury",
-  LENDING = "Lending",
 };
 
 export const CATEGORY_STABLE = "Stable";
