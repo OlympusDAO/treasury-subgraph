39c5ee6d177599b157c40feb009f7daab008635c
Jem <0x0xjem@gmail.com>
2023-09-27 12:21:14 +0400

    Add caching for earliest operations

--- apps/server/.wundergraph/operations/earliest/metrics.ts
+++ apps/server/.wundergraph/operations/earliest/metrics.ts
@@ -1,17 +1,32 @@
-import { createOperation } from '../../generated/wundergraph.factory';
+import { createOperation, z } from '../../generated/wundergraph.factory';
 import { Metric, getMetricObject } from '../../metricHelper';
 import { getBlockByChain } from '../../tokenRecordHelper';
 import { CHAIN_ARBITRUM, CHAIN_ETHEREUM, CHAIN_FANTOM, CHAIN_POLYGON } from '../../constants';
+import { getCacheKey, getCachedData, setCachedData } from '../../cacheHelper';
 
 /**
  * This custom query will return the earliest Metric object.
  */
 export default createOperation.query({
+  input: z.object({
+    ignoreCache: z.boolean({ description: "If true, ignores the cache and queries the subgraphs directly." }).optional(),
+  }),
   handler: async (ctx) => {
-    console.log(`Commencing earliest query for Metric`);
+    const FUNC = "earliest/metrics";
+    console.log(`${FUNC}: Commencing earliest query for Metric`);
+
+    // Return cached data if it exists
+    const cacheKey = getCacheKey(FUNC, ctx.input);
+    if (!ctx.input.ignoreCache) {
+      const cachedData = await getCachedData<Metric>(cacheKey);
+      if (cachedData) {
+        return cachedData;
+      }
+    }
 
     // Get the earliest block for each blockchain
     // TODO what if the earliest date is missing cross-chain data?
+    console.log(`${FUNC}: No cached data found, querying subgraphs...`);
     const latestQueryResult = await ctx.operations.query({
       operationName: "earliest/tokenRecords",
     });
@@ -48,6 +63,12 @@ export default createOperation.query({
     });
 
     const metricRecord: Metric | null = getMetricObject(tokenRecordsQueryResult.data || [], tokenSuppliesQueryResult.data || [], protocolMetricsQueryResult.data || []);
+
+    // Update the cache
+    if (metricRecord) {
+      await setCachedData<Metric>(cacheKey, metricRecord);
+    }
+
     return metricRecord;
   },
 });
--- apps/server/.wundergraph/operations/earliest/tokenSupplies.ts
+++ apps/server/.wundergraph/operations/earliest/tokenSupplies.ts
@@ -1,26 +1,45 @@
-import { createOperation } from '../../generated/wundergraph.factory';
-import { flattenRecords } from '../../tokenSupplyHelper';
+import { getCacheKey, getCachedData, setCachedData } from '../../cacheHelper';
+import { createOperation, z } from '../../generated/wundergraph.factory';
+import { TokenSupply, flattenRecords } from '../../tokenSupplyHelper';
 
 /**
  * This custom query will return a flat array containing the latest TokenSupply objects for
  * each endpoint.
  */
 export default createOperation.query({
+  input: z.object({
+    ignoreCache: z.boolean({ description: "If true, ignores the cache and queries the subgraphs directly." }).optional(),
+  }),
   handler: async (ctx) => {
-    console.log(`Commencing earliest query for TokenSupply`);
+    const FUNC = "earliest/tokenSupplies";
+    console.log(`${FUNC}: Commencing earliest query for TokenSupply`);
 
+    // Return cached data if it exists
+    const cacheKey = getCacheKey(FUNC, ctx.input);
+    if (!ctx.input.ignoreCache) {
+      const cachedData = await getCachedData<TokenSupply[]>(cacheKey);
+      if (cachedData) {
+        return cachedData;
+      }
+    }
+
+    console.log(`${FUNC}: No cached data found, querying subgraphs...`);
     const queryResult = await ctx.operations.query({
       operationName: "tokenSuppliesEarliest",
     });
 
     if (!queryResult.data) {
-      console.log(`No data returned.`);
+      console.log(`${FUNC}: No data returned.`);
       return [];
     }
 
     // Combine across pages and endpoints
     const flatRecords = flattenRecords(queryResult.data, true, false);
-    console.log(`Returning ${flatRecords.length} records.`);
+
+    // Update the cache
+    await setCachedData<TokenSupply[]>(cacheKey, flatRecords);
+
+    console.log(`${FUNC}: Returning ${flatRecords.length} records.`);
     return flatRecords;
   },
 });
